137dd77c9d7750547842b8880a32ead75cadc6bf
chars3 <charlesjoliveira65@gmail.com>
2025-05-07 17:03:27 -0300

    feat: setup global styles and base aplication structure

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -10,6 +10,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme.yellow};
   color: ${props => props.theme.white};
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
 `
---
+++ src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background:  ${props => props.theme.background};
+    color: ${props => props.theme["base-title"]};
+  }
+
+  h1, h2, h3  {
+    font-family: 'Baloo', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button {
+    background-color: ${props => props.theme["base-button"]};
+  }
+
+`

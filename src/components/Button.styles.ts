c702d1b0d84698287a9d5755d21b4e4a822b06fa
chars3 <charlesjoliveira65@gmail.com>
2025-05-07 16:25:44 -0300

    (feat) add initial project structure with custom theme typing

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant,
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+`
